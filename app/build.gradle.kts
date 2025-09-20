1e21421bfe8e2bc5f3ebf1fbd3ee0528d8fa6843
Karol Furtak <karol@MacBook-Pro-Karol.local>
2025-09-20 19:45:21 +0200

    add apollo3 runtime lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.lifecycle.viewmodel.navigation3)
     implementation(libs.kotlinx.serialization.core)
+    implementation(libs.apollo.runtime)
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
 
