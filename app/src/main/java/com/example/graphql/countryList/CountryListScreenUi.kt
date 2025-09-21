5d36cb526119c4d990226428cee28d093cd4a5b3
Karol Furtak <karol@MacBook-Pro-Karol.local>
2025-09-21 18:15:09 +0200

    display fetched country list

--- app/src/main/java/com/example/graphql/countryList/CountryListScreenUi.kt
+++ app/src/main/java/com/example/graphql/countryList/CountryListScreenUi.kt
@@ -2,30 +2,40 @@ package com.example.graphql.countryList
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun CountryListScreenUi(
     modifier: Modifier = Modifier,
     onItemClick: (Int) -> Unit
 ) {
+    val viewModel: CountryListViewModel = viewModel()
+
+    val list = viewModel.countries.collectAsStateWithLifecycle().value
+
     LazyColumn(
-        modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize()
     ) {
-        items(100) {
+        items(
+            items = list,
+            key = { it.code }
+        ) { country ->
             Text(
-                "note $it", Modifier
+                text = country.name + country.code + country.emoji,
+                modifier = Modifier
+                    .fillMaxWidth()
                     .padding(20.dp)
-                    .clickable(
-                        onClick = {
-                            onItemClick(it)
-                        }
-                    ))
+                    .clickable { onItemClick(country.code.toInt()) }
+            )
         }
     }
 }
---
+++ app/src/main/java/com/example/graphql/countryList/CountryListViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.graphql.countryList
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.graphql.domain.CountryListItem
+import com.example.graphql.domain.repo.CountryRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class CountryListViewModel @Inject constructor(
+    private val repository: CountryRepository
+): ViewModel() {
+
+    val countries: StateFlow<List<CountryListItem>> = flow {
+        emit(repository.getCountries())
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.Lazily,
+        initialValue = emptyList()
+    )
+}
